19d997f8a2c181774a1e409bd42639540699e8dc
Ali Adnan <aliadnanciit@gmail.com>
2021-07-06 02:00:06 +0400

    extract network module from appModule

--- app/src/main/java/de/westwing/campaignbrowser/di/AppModule.kt
+++ app/src/main/java/de/westwing/campaignbrowser/di/AppModule.kt
@@ -1,27 +1,15 @@
 package de.westwing.campaignbrowser.di
 
-import android.content.Context
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import de.westwing.campaignbrowser.R
 import de.westwing.campaignbrowser.repository.CampaignRepository
 import de.westwing.campaignbrowser.repository.CampaignRepositoryImpl
-import de.westwing.campaignbrowser.service.ApiInterface
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Named
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -33,48 +21,6 @@ abstract class AppModule {
 
 
     companion object {
-        @Provides
-        @Named("baseUrl")
-        fun baseUrl(@ApplicationContext context: Context): String {
-            return context.getString(R.string.base_url)
-        }
-
-        @Provides
-        @Singleton
-        fun okHttpClient(): OkHttpClient {
-            val loggingInterceptor = HttpLoggingInterceptor()
-            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-
-            val builder = OkHttpClient.Builder()
-            builder.connectTimeout(20, TimeUnit.SECONDS)
-                .writeTimeout(20, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .addInterceptor(loggingInterceptor)
-            return builder.build()
-        }
-
-        @Provides
-        @Singleton
-        fun moshi(): Moshi {
-            return Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
-        }
-
-        @Provides
-        fun provideApiInterface(
-            @Named("baseUrl") baseUrl: String,
-            moshi: Moshi
-        ): ApiInterface {
-            val retrofit = Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .client(okHttpClient())
-                .build()
-
-            return retrofit.create(ApiInterface::class.java)
-        }
-
         @Provides
         @Named("IO_DISPATCHER")
         fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
---
+++ app/src/main/java/de/westwing/campaignbrowser/di/NetworkModule.kt
@@ -0,0 +1,68 @@
+package de.westwing.campaignbrowser.di
+
+import android.content.Context
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import de.westwing.campaignbrowser.R
+import de.westwing.campaignbrowser.service.ApiInterface
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class NetworkModule {
+
+    companion object {
+        @Provides
+        @Named("baseUrl")
+        fun baseUrl(@ApplicationContext context: Context): String {
+            return context.getString(R.string.base_url)
+        }
+
+        @Provides
+        @Singleton
+        fun okHttpClient(): OkHttpClient {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val builder = OkHttpClient.Builder()
+            builder.connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+//                .addInterceptor(loggingInterceptor)
+            return builder.build()
+        }
+
+        @Provides
+        @Singleton
+        fun moshi(): Moshi {
+            return Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+        }
+
+        @Provides
+        fun provideApiInterface(
+            @Named("baseUrl") baseUrl: String,
+            moshi: Moshi
+        ): ApiInterface {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .client(okHttpClient())
+                .build()
+
+            return retrofit.create(ApiInterface::class.java)
+        }
+    }
+}
